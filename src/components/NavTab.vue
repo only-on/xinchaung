3dcbc02fc9664a339be78acdf9a895d607172aa5
caoliang <caoliang@simpleedu.com.cn>
2021-08-17 17:07:50 +0800

    refactor: 修改并在精品课页面引入NavTab

--- src/components/NavTab.vue
+++ src/components/NavTab.vue
@@ -1,55 +1,63 @@
 <template>
- <div class="navList">
-        <div class="tab">
-          <div v-for="v in tabs" :key="v.name" :class="activeName ===v.name?'active':''" @click="activeName !==v.name?tabChange(v):''">{{v.name}}</div>
-        </div>
-        <Breadcrumb />
+  <div class="navList">
+    <div class="tab">
+      <div
+        v-for="v in tabs"
+        :key="v.name"
+        :class="activeName === v.name ? 'active' : ''"
+        @click="activeName !== v.name ? tabChange(v) : ''"
+      >
+        {{ v.name }}
       </div>
+    </div>
+    <Breadcrumb />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent,ref, onMounted ,reactive, Ref} from 'vue'
-import {useStore} from "vuex"
-interface Tab {
-  name: string
+import { defineComponent, ref, onMounted, reactive, Ref } from "vue";
+import { useStore } from "vuex";
+
+export declare interface ITab {
+  name: string;
+  componenttype: number;
 }
+
 export default defineComponent({
-  name: 'NavTab',
-  props:{
-    tabs:{
+  name: "NavTab",
+  props: {
+    tabs: {
       required: false,
       type: Array,
-      default: () => [{name:'21'}],
-    }
+      default: () => [{ name: "21" }],
+    },
   },
-  emits:["tabSwitch"],
-  setup: (props,context) => {
-    const tabs:Tab[]=reactive(props.tabs) as Tab[]
-    const activeName=ref((tabs as any)[0].name)
-    function tabChange(item:any){
-      context.emit('tabSwitch',item)
-      activeName.value=item.name
+  emits: ["tabSwitch"],
+  setup: (props, context) => {
+    const tabs = reactive(props.tabs) as ITab[];
+    const activeName = ref(tabs[0].name);
+    function tabChange(item: ITab) {
+      context.emit("tabSwitch", item);
+      activeName.value = item.name;
     }
-  
-    onMounted(()=>{
-     
-    })
-    return {tabs ,activeName,tabChange};
+
+    onMounted(() => {});
+    return { tabs, activeName, tabChange };
   },
-})
+});
 </script>
 
 <style scoped lang="less">
-.navList{
+.navList {
   width: 1330px;
   margin: 0 auto;
   display: flex;
   justify-content: space-between;
-  border-bottom: 1px solid #D0D0D0;
+  border-bottom: 1px solid #d0d0d0;
   line-height: 43px;
-  .tab{
+  .tab {
     display: flex;
-    div{
+    div {
       border-top-left-radius: 5px;
       border-top-right-radius: 5px;
       margin-right: 15px;
@@ -58,14 +66,14 @@ export default defineComponent({
       padding: 0 15px;
       text-align: center;
       cursor: pointer;
-      &:hover{
+      &:hover {
         color: #999;
       }
     }
-    .active{
-       background:  @primary-color;
-       color: #fff;
-       &:hover{
+    .active {
+      background: @primary-color;
+      color: #fff;
+      &:hover {
         color: #fff;
       }
     }
--- src/views/classicalAsset/Panel.vue
+++ src/views/classicalAsset/Panel.vue
@@ -1,12 +1,22 @@
 <template>
+  <nav-tab :tabs="tabs" @tabSwitch="tabSwitch" />
   <p>资源面板</p>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 import http from "src/api/index";
+import NavTab, { ITab } from "src/components/NavTab.vue";
 export default defineComponent({
   name: "ClassicalAssetPanel",
+  components: {
+    NavTab
+  },
   setup() {
+    const tabs = [
+      { name: "公有", componenttype: 0 },
+      { name: "私有", componenttype: 1 },
+    ];
+
     http.classicalAsset.datasetList({
       param: {
         type: 3,
@@ -19,7 +29,11 @@ export default defineComponent({
         console.log("got resp: ", resp);
       },
     });
-    return {};
+
+    const tabSwitch = (tab: ITab) => {
+      console.log(tab);
+    };
+    return { tabs, tabSwitch };
   },
 });
 </script>
