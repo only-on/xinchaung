340ac0446fe83bdf078c4b71daddcaad71e3b30a
caoliang <caoliang@simpleedu.com.cn>
2021-08-19 16:37:49 +0800

    feat: 增加目录创建信息录入框

--- src/views/classicalAsset/Panel.vue
+++ src/views/classicalAsset/Panel.vue
@@ -11,7 +11,7 @@
           placeholder="请输入目录名称关键字查询"
           @search="handleSearch"
         />
-        <a-button type="primary">
+        <a-button type="primary" @click="createFolder">
           <template #icon
             ><span class="iconfont icon-tianjia"></span>&nbsp;</template
           >
@@ -21,10 +21,41 @@
       <asset-folder></asset-folder>
       <asset-folder></asset-folder>
     </div>
+    <a-modal
+      v-model:visible="createFolderVisible"
+      title="创建目录"
+      @ok="handleCreateFolder"
+    >
+      <a-form
+        :model="folderInfo"
+        :label-col="labelCol"
+        :wrapper-col="wrapperCol"
+      >
+        <a-form-item label="名称：">
+          <a-input
+            placeholder="输入名称"
+            v-model:value="folderInfo.name"
+            :showCount="true"
+            :maxlength="60"
+          >
+            <template #suffix> 0/30 </template>
+          </a-input>
+        </a-form-item>
+        <a-form-item label="描述：">
+          <a-textarea
+            rows="1"
+            placeholder="请输入描述"
+            v-model:value="folderInfo.description"
+            showCount
+            :maxlength="100"
+          />
+        </a-form-item>
+      </a-form>
+    </a-modal>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent, ref, reactive } from "vue";
 // import http from "src/api/index";
 import { ITab } from "src/components/NavTab.vue";
 import AssetFolder from "../../components/classical/AssetFolder.vue";
@@ -38,6 +69,14 @@ export default defineComponent({
   setup() {
     const searchStr = ref("");
     const showDiskUsage = ref(false);
+    const createFolderVisible = ref(false);
+    const folderInfo = reactive({
+      name: "",
+      description: "",
+    });
+    const labelCol = { span: 3 };
+    const wrapperCol = { span: 21 };
+
     const tabs = [
       { name: "公有", componenttype: 0 },
       { name: "私有", componenttype: 1 },
@@ -62,7 +101,25 @@ export default defineComponent({
     const handleSearch = (searchValue: string) => {
       console.log("[classical][panel] searching: ", searchValue);
     };
-    return { tabs, searchStr, showDiskUsage, tabSwitch, handleSearch };
+    const createFolder = () => {
+      createFolderVisible.value = true;
+    };
+    const handleCreateFolder = () => {
+      createFolderVisible.value = false;
+    };
+    return {
+      tabs,
+      searchStr,
+      showDiskUsage,
+      createFolderVisible,
+      folderInfo,
+      labelCol,
+      wrapperCol,
+      tabSwitch,
+      handleSearch,
+      createFolder,
+      handleCreateFolder,
+    };
   },
 });
 </script>
