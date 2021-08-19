8b2395b095a26c7a9a42b6db4ed34747331eeb3d
caoliang <caoliang@simpleedu.com.cn>
2021-08-19 11:10:34 +0800

    feat: 初步完成教师端精品课资源文件目录列表页静态页面

---
+++ src/components/classical/AssetFolder.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="classical__asset-folder">
+    <div class="classical__asset-main">
+      <div class="classical__asset-title">title</div>
+      <div class="classical__asset-detail">
+          <ul>
+              <li><span class="iconfont icon-shijian"></span>2021-07-12</li>
+              <li><span class="iconfont icon-cunchuzhi"></span>10.2MB</li>
+              <li><span class="iconfont icon-wenjian"></span>3个</li>
+          </ul>
+      </div>
+    </div>
+    <div class="classical__asset-right">
+      <span class="iconfont icon-shanchu"></span>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "AssetFolder",
+});
+</script>
+<style lang="less">
+.classical__asset-folder {
+  border: 1px solid @normal-color;
+  border-radius: @border-radius-card;
+  padding: @padding-sm;
+  display: flex;
+  justify-content: space-between;
+  // https://developer.mozilla.org/zh-CN/docs/Web/CSS/CSS_Box_Model/Mastering_margin_collapsing
+  // 相同块元素之间，在垂直方向上，其margin会叠加，css这样设计的意义在于避免产生双倍边距
+  margin: @margin-md 0px;
+  color: @text-color-secondary;
+
+  &:hover {
+      box-shadow: @shadow-ring;
+  }
+
+  .classical__asset-title {
+    font-size: @font-size-lg;
+    color: @text-color;
+  }
+
+  .classical__asset-right {
+    line-height: 53px;
+  }
+
+  .classical__asset-detail {
+      ul {
+          margin: 0;
+          padding: 0;
+          li {
+              display: inline-block;
+              margin-right: @margin-md;
+          }
+      }
+  }
+}
+</style>
--- src/views/classicalAsset/Panel.vue
+++ src/views/classicalAsset/Panel.vue
@@ -1,39 +1,83 @@
 <template>
-  <nav-tab :tabs="tabs" @tabSwitch="tabSwitch" />
-  <p>资源面板</p>
+  <div class="classical__container">
+    <a-card class="classical__card">
+      <nav-tab :tabs="tabs" @tabSwitch="tabSwitch" style="width: auto" />
+      <div class="classical__search-form">
+        <a-input-search
+          class="classical_search-input"
+          v-model:value="searchStr"
+          placeholder="请输入目录名称关键字查询"
+          @search="handleSearch"
+        />
+        <a-button type="primary">
+          <template #icon
+            ><span class="iconfont icon-tianjia"></span>&nbsp;</template
+          >
+          创建目录
+        </a-button>
+      </div>
+      <asset-folder></asset-folder>
+      <asset-folder></asset-folder>
+    </a-card>
+  </div>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
-import http from "src/api/index";
-import NavTab, { ITab } from "src/components/NavTab.vue";
+import { defineComponent, ref } from "vue";
+// import http from "src/api/index";
+import { ITab } from "src/components/NavTab.vue";
+import AssetFolder from "../../components/classical/AssetFolder.vue";
 export default defineComponent({
   name: "ClassicalAssetPanel",
   components: {
-    NavTab
+    AssetFolder,
   },
   setup() {
+    const searchStr = ref("");
     const tabs = [
       { name: "公有", componenttype: 0 },
       { name: "私有", componenttype: 1 },
     ];
 
-    http.classicalAsset.datasetList({
-      param: {
-        type: 3,
-        page: 1,
-        pageSize: 10,
-        is_public: 1,
-        name: "",
-      },
-      success: (resp) => {
-        console.log("got resp: ", resp);
-      },
-    });
+    // http.classicalAsset.datasetList({
+    //   param: {
+    //     type: 3,
+    //     page: 1,
+    //     pageSize: 10,
+    //     is_public: 1,
+    //     name: "",
+    //   },
+    //   success: (resp) => {
+    //     console.log("got resp: ", resp);
+    //   },
+    // });
 
     const tabSwitch = (tab: ITab) => {
       console.log(tab);
     };
-    return { tabs, tabSwitch };
+    const handleSearch = (searchValue: string) => {
+      console.log("[classical][panel] searching: ", searchValue);
+    };
+    return { tabs, searchStr, tabSwitch, handleSearch };
   },
 });
 </script>
+<style lang="less">
+.classical__container {
+  width: @center-width;
+  margin: 0 auto;
+  min-height: 750px;
+  .classical__card {
+    width: 100%;
+    height: 100%;
+    .classical__search-form {
+      margin-top: @margin-md;
+      padding: @padding-md 0px;
+      display: flex;
+      justify-content: space-between;
+      .classical_search-input {
+        width: 50%;
+      }
+    }
+  }
+}
+</style>
