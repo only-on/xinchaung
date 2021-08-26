e9f03cc5c35f5249034dcb141a2a5270ebb66661
caoliang <caoliang@simpleedu.com.cn>
2021-08-26 15:46:33 +0800

    fix: 解决项目初始化代码中，markdown包导入路径错误的问题

--- src/utils/project.ts
+++ src/utils/project.ts
@@ -6,7 +6,7 @@ import router from "../routers/index";
 import i18n from '../i18n'
 import store from "../store/index";
 import customDirective from '../directive'
-import { registration as AntdvMarkdownRegistration } from 'packages/antdv-markdown/src/index';
+import { registration as AntdvMarkdownRegistration } from '@jiangyue/antdv-markdown/src/index';
 
 export declare interface IAppManager {
     // 安装插件，暂不支持设置options
