d5d598b4828ed405a725844367d5a20c0cf65e84
caoliang <caoliang@simpleedu.com.cn>
2021-11-25 17:58:01 +0800

    fix: 更新markdown组件，支持上传图片了

--- src/components/dev/Markdown.vue
+++ src/components/dev/Markdown.vue
@@ -32,6 +32,10 @@
         <antdv-markdown
           v-model="content3"
           :preview-only="previewOnly"
+          :image-upload-url="'/proxyPrefix/api/instance/uploads/file'"
+          :image-upload-before-hook="handleBeforeUpload"
+          :image-upload-data="imageUploadData"
+          :image-upload-headers="imageUploadHeaders"
           class="markdown__editor"
         />
       </div>
@@ -57,7 +61,12 @@ export default defineComponent({
       '#demo\n不标准的markdown语法，支持html\n<center>第一个居中</center><center>\n第二个居中</center><center>第三个居中\n</center>\n<p style="background: blue; color: white">来一段蓝色背景文字</p>'
     );
     const content3 = ref("#仅预览模式");
-    const previewOnly = ref(true);
+    const previewOnly = ref(false);
+    const imageUploadData = {
+      upload_path: 'image/test', default_name: 0
+    };
+    const imageUploadHeaders = {'X-Auth': 'xudjn82fh81gf0234..0'}
+
     watch(content, (newVal) => {
       console.log("[markdown] content changed");
     });
@@ -65,7 +74,41 @@ export default defineComponent({
     const togglePreview = () => {
       previewOnly.value = !previewOnly.value;
     };
-    return { content, content2, content3, previewOnly, togglePreview };
+
+    const handleUpload = (file: object) => {
+      // console.log("uploading...", file);
+      // const fd = new FormData();
+      // fd.append("file", file.file);
+      // fd.append("upload_path", props.uploadPathName);
+      // fd.append("default_name", "1");
+      // await http.uploadsFile({ param: fd }).then((res: IBusinessResp) => {
+      //   let html = `<img src="${dev_base_url}${res.data.url}" alt="">`;
+      //   // let html= `<img src="${res.data.url}" alt="">`
+      //   //  console.log(html);
+      //   insertHtml(html);
+      // });
+    };
+
+    const handleBeforeUpload = (file: File, fileList: File[]) => {
+      console.log(
+        "[handleBeforeUpload] file: ",
+        file,
+        ", fileList: ",
+        fileList
+      );
+      return true;
+    };
+    return {
+      content,
+      content2,
+      content3,
+      previewOnly,
+      imageUploadData,
+      imageUploadHeaders,
+      togglePreview,
+      handleUpload,
+      handleBeforeUpload
+    };
   },
 });
 </script>
