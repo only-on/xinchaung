f3a75223bb521e11fc34a26e351ea97614be0b80
wangting <wangting@simpleedu.com.cn>
2022-02-21 17:56:58 +0800

    fix: markdown上传图片

--- src/components/editor/markedEditor.vue
+++ src/components/editor/markedEditor.vue
@@ -1,35 +1,71 @@
 <template>
   <div class="demo__container">
-    <AntdvMarkdown editor-name="content"  :preview-only="preview" v-model="content"/>
+    <antdv-markdown
+      editor-name="content"
+      :image-upload-url="
+        env
+          ? '/proxyPrefix/api/content/vnc/upload_mkfile'
+          : '/api/content/vnc/upload_mkfile'
+      "
+      :image-upload-before-hook="handleBeforeUpload"
+      :image-upload-data="imageUploadData"
+      :image-upload-headers="imageUploadHeaders"
+      :preview-only="preview"
+      v-model="content"
+    />
   </div>
 </template>
 <script lang="ts">
-  import { defineComponent,watch,ref } from 'vue'
-  export default defineComponent({
-    name: 'DemoMarkedEditor',
-    components: {
-    },
-    props:['modelValue','preview'],
-    setup(props,{emit}){
-        // const preview=props.preview
-        const content=ref("")
-        watch(()=>props.modelValue,()=>{
-            content.value=props.modelValue
-        },{deep:true,immediate:true})
-        watch(()=>content.value,()=>{
-            emit("update:modelValue",content.value)
-        })
-        return{
-            // preview,
-            content
-        }
-    }
-  })
+import { defineComponent, watch, ref } from "vue";
+export default defineComponent({
+  name: "DemoMarkedEditor",
+  components: {},
+  props: ["modelValue", "preview"],
+  setup(props, { emit }) {
+    // const preview=props.preview
+    const content = ref("");
+    const env = process.env.NODE_ENV == "development" ? true : false;
+    watch(
+      () => props.modelValue,
+      () => {
+        content.value = props.modelValue;
+      },
+      { deep: true, immediate: true }
+    );
+    watch(
+      () => content.value,
+      () => {
+        emit("update:modelValue", content.value);
+      }
+    );
+    const handleBeforeUpload = (file: File, fileList: File[]) => {
+      console.log(
+        "[handleBeforeUpload] file: ",
+        file,
+        ", fileList: ",
+        fileList
+      );
+      return true;
+    };
+    const imageUploadData = {
+      // md_file: 'image/test',
+    };
+    const imageUploadHeaders = { "X-Auth": "xudjn82fh81gf0234..0" };
+    return {
+      // preview,
+      content,
+      handleBeforeUpload,
+      imageUploadData,
+      imageUploadHeaders,
+      env,
+    };
+  },
+});
 </script>
 <style lang="less" scoped>
-  .demo__container {
-    width: 100%;
-    height: 100%;
-    margin: 0 auto;
-  }
+.demo__container {
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+}
 </style>
