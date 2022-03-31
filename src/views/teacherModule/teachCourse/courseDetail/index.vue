2ed3742ecad753ed6b83d2c0762d714e30d57c4d
tianfeng <talongsoft@163.com>
2022-03-31 18:08:33 +0800

    fix: xiufu

--- src/views/teacherModule/teachCourse/courseDetail/index.vue
+++ src/views/teacherModule/teachCourse/courseDetail/index.vue
@@ -1,6 +1,6 @@
 <template>
   <DetailHeader :info="{}" :tabs="Number(currentTab)===0?detailTabs:[]" @selectTab="selectTab" />
-  <div class="courseContent">
+  <div class="courseContent" v-if="false">
     <div class="leftContent content">
       <h3 class="courseH3">课程简介</h3>
       <div class="introduce">Wiki是一种在网络上开放且可供多人协同创作的超文本系统，由美国人沃德·坎宁安于1995年首先开发，这种超文本系统支持面向社群的协作式写作，同时也包括一组支持这种写作。沃德·坎宁安将wiki定义为“一种允许一群用户用简单的描述来创建和连接一组网页的社会计算系统”。 [1] Wiki站点可以有多人（甚至任何访问者）维护，每个人都可以发表自己的意见。</div>
@@ -77,9 +77,9 @@
     </div>
   </div>
   <div>
-
+    <!-- 每个tab对应的组件 -->
   </div>
-  
+
   <a-modal v-model:visible="Visible" title="知识点" :width="1330" class="modal-post" :destroyOnClose="true">
     <div id="KnowledgePoints">
 
