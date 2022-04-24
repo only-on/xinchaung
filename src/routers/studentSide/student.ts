4935f2252fe8f4102c7bcff655322479d61a5b57
Rex Tsao <cl0067@126.com>
2022-04-24 18:34:57 +0800

    refactor: 学生首页增加header

--- src/routers/studentSide/student.ts
+++ src/routers/studentSide/student.ts
@@ -1,10 +1,22 @@
+import Layout from "src/views/common/Layout.vue";
+
 export default {
   path: "statistics",
-  component: () => import("src/views/studentModule/student/index.vue"),
-  name:'studentStatistics',
+  component: Layout,
+  name: "studentStatistics",
   meta: {
-    title:'学生端首页',
-    authCode: 'studentStatistics'
+    title: "学生端首页",
+    authCode: "studentStatistics",
   },
-  children: []
-}
+  children: [
+    {
+      path: "",
+      name: "",
+      component: () => import("src/views/studentModule/student/index.vue"),
+      meta: {
+        title: "学生端首页",
+        authCode: "studentStatistics",
+      },
+    },
+  ],
+};
