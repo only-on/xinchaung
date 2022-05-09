2517e743f5da117f963e65d09c8f3b1e51832dd7
wangting <wangting@simpleedu.com.cn>
2022-05-09 15:33:27 +0800

    fix: 素材资源管理页面

---
+++ src/views/adminModule/TeachingResourceManagement/components/StatisticsPie.vue
@@ -0,0 +1,118 @@
+<template>
+  <div id="statistic-pie" style="width=100%;height: 100%;"></div>
+</template>
+
+<script lang="ts" setup>
+import { ref, toRefs, onMounted,inject,reactive, watch} from "vue";
+import * as echarts from "echarts"
+interface IData {
+  name: string
+  value: number
+}
+const props = withDefaults(defineProps<{title: string;data: IData[]}>(), {
+  title: '素材资源统计',
+  data: () =>  [{ value:10, name: '私有资源' }, { value:5, name: '公开资源' }]
+});
+const drawEcharts = () => {
+  const option={
+    color:['#1cb2b3','#ff9544',],
+    title: {
+      text: props.title,
+      left: '0%',
+      top:'0%', 
+      textStyle: {
+        color: 'rgba(0, 0, 0, 0.85)',
+        fontWeight: 'normal',
+        fontSize: 16
+      }
+    },
+    tooltip: {
+      trigger: 'item',
+    },
+    legend: {
+      show: true,
+      icon: "circle",
+      top: "30%",
+      left: "0%",
+      width: 50,
+      // padding: [0, 5],
+      itemGap: 15,
+      itemHeight: 6,
+      orient: "vertical",
+      // top: '35%',
+      // left: '0%',
+      formatter: function (name:any) {
+        var data = option.series[0].data;
+        var total = 0;
+        var tarValue;
+        for (var i = 0; i < data.length; i++) {
+          total += data[i].value;
+          if (data[i].name == name) {
+            tarValue = data[i].value;
+          }
+        }
+        var value = tarValue;
+        return [
+          `{name|${name}}`,
+          `{value|${value}}`
+        ].join('\n')
+      },
+      textStyle:{
+        rich:{
+          name:{
+            fontSize:12,
+            color:"#808294",
+            // lineHeight:30,
+          },
+          value:{
+            fontSize:20,
+            color:"#102241",
+            // lineHeight:20,
+            fontWeight: "bold",
+            padding: [5, 0],
+          }
+        }
+      },
+    },
+    series: [
+      {
+        name: '',
+        type: 'pie',
+        center: ['68%', '50%'],
+        radius: ['60%', '80%'],
+        // roseType: 'radius',
+        avoidLabelOverlap: false,
+        label: {
+          show: false,
+          position: 'center'
+        },
+        emphasis: {
+          label: {
+            show:false,
+            fontSize: '16',
+            fontWeight: 'bold'
+          }
+        },
+        labelLine: {
+          show: false
+        },
+        data: props.data
+      }
+    ]
+  }
+  const chartDom:any = document.getElementById("statistic-pie")
+  const chart = chartDom && echarts?.init(chartDom)
+  chart && chart.setOption(option);
+}
+onMounted(() => {
+  drawEcharts()
+})
+watch(
+  ()=> props.data, 
+  () => {
+    drawEcharts()
+  },{
+    deep: true, immediate: true
+  }
+)
+</script>
--- src/views/adminModule/TeachingResourceManagement/resourcesManagement/resourcesManagement.vue
+++ src/views/adminModule/TeachingResourceManagement/resourcesManagement/resourcesManagement.vue
@@ -1,12 +1,337 @@
 <template>
-  <div>资源管理</div>
+  <div class="resourceManagement">
+    <div class="Statistics flexCenter">
+      <div class="item">
+        <statistics-pie title="素材资源统计" :data="statisticList"></statistics-pie>
+      </div>
+      <div class="item item2">
+        <h3 class="statisticalTitle">素材类型</h3>
+        <div class="progressBox flexCenter">
+          <div class="progressItem progressItem1" v-for="list in materialTypeList" :key="list.id" :class="list.class">
+            <div class="text flexCenter">
+              <span :class="['iconfont', list.icon]" ></span>
+              <span class="num">{{list.count}}</span>
+            </div>
+            <div class="type">{{list.name}}</div>
+          </div>
+          <!-- <div class="progressItem progressItem2">
+            <div class="text flexCenter">
+              <span class="iconfont icon-docker"></span>
+              <span class="num">{{analysisObj.dockerImagesCount}}</span>
+            </div>
+            <div class="type">Docker</div>
+          </div>
+          <div class="progressItem progressItem3">
+            <div class="text flexCenter">
+              <span class="iconfont icon-linux"></span>
+              <span class="num">{{analysisObj.kvmLinuxImagesCount}}</span>
+            </div>
+            <div class="type">KVM-Linux</div>
+          </div>
+          <div class="progressItem progressItem4">
+            <div class="text flexCenter">
+              <span class="iconfont icon-windows"></span>
+              <span class="num">{{analysisObj.kvmWindowImagesCount}}</span>
+            </div>
+            <div class="type">KVM-Windows</div>
+          </div>
+          <div class="progressItem progressItem1">
+            <div class="text flexCenter">
+              <span class="iconfont icon-linux"></span>
+              <span class="num">{{analysisObj.kvmLinuxImagesCount}}</span>
+            </div>
+            <div class="type">KVM-Linux</div>
+          </div>
+          <div class="progressItem progressItem2">
+            <div class="text flexCenter">
+              <span class="iconfont icon-windows"></span>
+              <span class="num">{{analysisObj.kvmWindowImagesCount}}</span>
+            </div>
+            <div class="type">KVM-Windows</div>
+          </div> -->
+        </div>
+      </div>
+    </div>
+    <div class="courseManagementContent">
+      <div class="search flexCenter">
+        <div class="left flexCenter">
+          <div class="item">
+            <span>素材名称：</span>
+            <a-input v-model:value="searchInfo.imageName" placeholder="请输入关键字搜索" @keyup.enter="searchList"  />
+          </div>
+          <div class="item">
+            <span>素材属性：</span>
+            <a-select v-model:value="searchInfo.imageAttr" placeholder="请选择素材属性"  @change="courseStatechange">
+              <a-select-option :value="0">公开素材</a-select-option>
+              <a-select-option :value="1">私有素材</a-select-option>
+            </a-select>
+          </div>
+          <div class="item">
+            <span>素材类型：</span>
+            <a-select v-model:value="searchInfo.imageType" placeholder="请选择素材类型"  @change="courseStatechange">
+              <a-select-option value="">全部</a-select-option>
+              <a-select-option  v-for="list in materialTypeList" :key="list.id" :value="list.name">{{list.name}}</a-select-option>
+              <!-- <a-select-option value="Docker">Docker</a-select-option>
+              <a-select-option value="KVM-Linux">KVM-Linux</a-select-option>
+              <a-select-option value="KVM-Windows">KVM-Windows</a-select-option> -->
+            </a-select>
+          </div>
+        </div>
+        <div class="right">
+          <a-button type="primary"> 批量删除 </a-button>
+        </div>
+      </div>
+      <div class="tableContent">
+        <a-spin :spinning="loading" size="large" tip="Loading...">
+          <a-config-provider>
+            <a-table :columns="columns" :data-source="courseList"
+              :pagination="{ hideOnSinglePage: false, total: totalCount, pageSize: searchInfo.limit,current: searchInfo.page, onChange: onChange}"
+              :row-selection="{ selectedRowKeys: searchInfo.selectedRowKeys, onChange: onSelectChange,}"
+              rowKey="id">
+              <template #imageTags="{ record }">
+                <div>{{(record.imageTags && record.imageTags.length)?`${record.imageTags.join(' / ')}`:''}}</div>
+              </template>
+            </a-table>
+            <template #renderEmpty>
+              <div v-if="!loading"><Empty type="tableEmpty" /></div>
+            </template>
+          </a-config-provider>
+        </a-spin>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, toRefs, onMounted ,Ref} from "vue";
+import { inject,ref, toRefs, onMounted ,Ref,reactive} from "vue";
+import request from "src/api/index";
+import { IBusinessResp } from "src/typings/fetch.d";
+import { ColumnProps } from "ant-design-vue/es/table/interface";
+import StatisticsPie from './../components/StatisticsPie.vue'
+const http = (request as any).TeachingResourceManagement;
+var updata = inject("updataNav") as Function;
+updata({
+  tabs: [{ name: "素材资源管理", componenttype: 0 }],
+  showContent: true,
+  componenttype: undefined,
+  showNav: true,
+});
+const materialTypeList: any = reactive([
+  {id: 1, name: "数据集", count: 9, class: 'progressItem1', icon: 'icon-shujuji'},
+  {id: 2, name: "视频", count: 9, class: 'progressItem2', icon: 'icon-shipinbofang'},
+  {id: 3, name: "课件", count: 9, class: 'progressItem3', icon: 'icon-a-zu8851'},
+  {id: 4, name: "备课资料", count: 9, class: 'progressItem4', icon: 'icon-a-zu8852'},
+  {id: 5, name: "教学指导", count: 9, class: 'progressItem1', icon: 'icon-a-zu8853'},
+  {id: 6, name: "实验文档", count: 9, class: 'progressItem2', icon: 'icon-a-zu8854'},
+])
+const columns= [
+  {
+    title: "目录名称",
+    dataIndex: "imageName",
+    align: "left",
+    ellipsis: true,
+  },
+  {
+    title: "素材属性",
+    dataIndex: "imageGroup",
+    align: "center",
+  },
+  {
+    title: "素材所属",
+    dataIndex: "imageGroup",
+    align: "center",
+  },
+  {
+    title: "素材类型",
+    dataIndex: "imageType",
+    align: "center",
+  },
+  {
+    title: "数量",
+    dataIndex: "imageSize",
+    align: "center",
+  },
+  {
+    title: "大小",
+    dataIndex: "imageSize",
+    align: "center",
+  },
+  {
+    title: "标签",
+    dataIndex: "imageTags",
+    align: "center",
+    ellipsis: true,
+    slots: { customRender: "imageTags" },
+  },
+  {
+    title: "描述",
+    dataIndex: "imageDescription",
+    align: "center",
+    ellipsis: true,
+  },
+]
+var searchInfo:any=reactive({
+  imageName:'',
+  imageGroup:'',
+  imageType:'',
+  imageAttr: 0,
+  page:1,
+  limit:10,
+  selectedRowKeys:[],
+  analysis:{}
+})
+var loading: Ref<boolean> = ref(false);
+var courseList: any[] = reactive([{id:1}]);
+var totalCount: Ref<number> = ref(0);
+var analysisObj:any=reactive({})
+const initData = () => {
+  const param:any={
+    'search[imageName]':searchInfo.imageName,
+    'search[imageGroup]':searchInfo.imageGroup,
+    'search[imageType]':searchInfo.imageType,
+    page:searchInfo.page,
+    limit:searchInfo.limit
+  }
+  loading.value = true;
+  courseList.length = 0
+  http.imageslist({param:{...param}}).then((res: IBusinessResp) => {
+    loading.value = false
+    if (!res) return
+    const { list, page,analysis }  = res.data
+    list.forEach((v: any) => {
+      // v.type_obj = Object.assign({}, getTypeList('90deg')[v.task_type]);
+    });
+    Object.assign(analysisObj,analysis)
+    courseList.push(...list)
+    totalCount.value = page.totalCount
+  })
+};
 
+const onChange=(page: any, pageSize: any)=> {
+  searchInfo.page=page
+  initData()
+}
+type Key = ColumnProps["key"];
+const onSelectChange=(selectedRowKeys: Key[], selectedRows: Key[])=> {
+  searchInfo.selectedRowKeys = selectedRowKeys; // 不去分别分页的弹窗已选ids
+  // state.selectedRows = selectedRows; // 弹窗当前页已选 list
+}
+const courseStatechange=(val: any)=> {
+  searchInfo.page=1
+  initData()
+}
+const searchList=()=> {
+  searchInfo.page=1
+  initData()
+}
+const statisticList: any = reactive([])
+onMounted(() => {
+  statisticList.push(...[{ value:100, name: '私有资源' }, { value:50, name: '公开资源' }])
+  initData()
+});
 </script>
 
 <style lang="less" scoped>
+  .statisticalTitle{
+    color: var(--black-85);
+    font-weight: 500;
+  }
+  .Statistics{
+    justify-content: space-between;
+    .item{
+      width: 285px;
+      height: 180px;
+      background-color: var(--white);
+      padding: 12px;
+    }
+    .item2{
+      width:895px;
+      .progressBox{
+        justify-content: space-between;
+        margin-top: 24px;
+        .progressItem{
+          width: 130px;
+          height: 85px;
+          border-radius: 6px;
+          padding: 12px 20px;
+          .text{
+            justify-content: space-between;
+            .iconfont{
+              font-size: 26px;
+            }
+            .num{
+              font-size: 28px;
+              color: #3a3b43;
+              font-weight: 400;
+            }
+          }
+          .type{
+            font-size: 12px;
+            color:#363D5F;
+            text-align: right;
+          }
+        }
+        .progressItem1{
+          background: linear-gradient(180deg,#fcdecb, #e9dbe4 55%, #dadaf8);
+        }
+        .progressItem2{
+          background: linear-gradient(180deg,#95edf3, #a4cbfb);
+        }
+        .progressItem3{
+          background: linear-gradient(180deg,#fbbcd9, #fecbd6 55%, #ffd2d5);
+        }
+        .progressItem4{
+          background: linear-gradient(180deg,#9badfb, #c8adfc 71%, #d5b3fc);
+        }
+      }
+    }
+    .item3{
+      .started{
+        display: flex;
+        padding-top: 1rem;
+        .num{
+         flex-grow: 1;
+         color:#31394D;
+         font-size: 30px;
+         font-weight: 600;
+        }
+        .img{
+          width: 164px;
+          height: 98px;
+          background-size: 100% 100%;
+          background-image: url('src/assets/images/user/teacher.png');
+          background-size: 100% 100%;
+          background-repeat: no-repeat;
+        }
+      }
+    }
+  }
+  .courseManagementContent{
+    background-color: var(--white);
+    margin-top: 2rem;
+    min-height: 750px;
+    padding: 24px 30px 0;
+    .search{
+      justify-content: space-between;
+      .left{
+        .item{
+          display: flex;
+          align-items: center;
+          margin-right: 12px;
+          .ant-select,.ant-input{
+            width: 200px;
+          }
+        }
+      }
+      .right{
+        .ant-btn:nth-child(1){
+          margin-right: 12px;
+        }
+      }
+    }
+    .tableContent{
+      margin-top: 2rem;
+    }
 
+  }
 </style>
