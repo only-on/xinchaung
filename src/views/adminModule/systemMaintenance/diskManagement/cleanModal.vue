77b90bda1e6624c4a942f75e3f2bb18287baf7e1
shiyao <shiyao@123>
2022-06-08 14:19:18 +0800

    fix: 修改
    Signed-off-by: shiyao <shiyao@123>

--- src/views/adminModule/systemMaintenance/diskManagement/cleanModal.vue
+++ src/views/adminModule/systemMaintenance/diskManagement/cleanModal.vue
@@ -2,43 +2,41 @@
     <div>
         <a-modal v-model:visible="visible" :width='480' :title="cleanType?'自动清理设置':(diskType==='video'?'录屏':'系统日志')" @cancel='handCancel'  @ok="handleOk">
             <div v-if="cleanType">
-                <div>
+                <div >
                     <div>单个节点磁盘使用量达到80%</div>
-                    自动清除系统中
-                    <!-- <before-icon :icon="selectIcon">
-                        <a-select style="width: 143px" v-model:value="value2" class="dev_state">
-                        <a-select-option
-                            v-for="item in timeSelectData"
-                            :value="item.value"
-                            :key="item.value"
-                            >{{ item.label }}</a-select-option
-                        >
-                        </a-select>
-                    以前的操作视频
-                    </before-icon> -->
-                    <a-select style="width: 143px" v-model:value="value2" class="dev_state">
-                        <a-select-option
-                            v-for="item in timeSelectData"
-                            :value="item.value"
-                            :key="item.value"
-                            >{{ item.label }}</a-select-option
-                        >
-                        </a-select>
+                    <div class="cnadelBox">
+                        自动清除系统中
+                        <before-icon class="select" :icon="selectIcon">
+                            <a-select style="width: 143px" v-model:value="value2">
+                            <a-select-option
+                                v-for="item in timeSelectData"
+                                :value="item.value"
+                                :key="item.value"
+                                >{{ item.label }}</a-select-option
+                            >
+                            </a-select>
+                        </before-icon>
                         日期当日及之前的<span>{{diskType==='video'?'操作视频':'系统日志'}}</span>
+                    </div>
+
                 </div>
             </div>
             <div v-else>
                 <div>
-                    请选择，清理
-                    <a-select style="width: 143px" v-model:value="value2" class="dev_state">
-                    <a-select-option
-                        v-for="item in timeSelectData"
-                        :value="item.value"
-                        :key="item.value"
-                        >{{ item.label }}</a-select-option
-                    >
-                    </a-select>
-                    日期当日及之前的<span>{{diskType==='video'?'操作视频':'系统日志'}}</span>
+                    <div class="cnadelBox">
+                        请选择，清理
+                        <before-icon class="select" :icon="selectIcon">
+                                <a-select style="width: 143px" v-model:value="value2">
+                                <a-select-option
+                                    v-for="item in timeSelectData"
+                                    :value="item.value"
+                                    :key="item.value"
+                                    >{{ item.label }}</a-select-option
+                                >
+                                </a-select>
+                        </before-icon>
+                        日期当日及之前的<span>{{diskType==='video'?'操作视频':'系统日志'}}</span>
+                </div>
                     <br>
                 （建议不要清除近三个月的内容）
                 </div>
@@ -56,7 +54,7 @@
     } from "vue";
     import moment from 'moment';
     import beforeIcon from "src/components/aiAnt/beforeIcon.vue";
-    import selectIcon from "src/assets/images/screenicon/Group14.png";
+    import selectIcon from "src/assets/images/screenicon/date.png";
     const timeSelectData=[
         {label:'一个月前',value:30},
         {label:'二个月前',value:60},
@@ -95,3 +93,15 @@ function handCancel(){
 }
 
 </script>
+<style lang="less" scoped>
+  .cnadelBox{
+      display: flex;
+      align-items: center;
+      margin-top: 10px;
+  } 
+  .select {
+      :deep(.ant-select-selector){
+        border-radius: 20px;
+      }
+  }
+</style>
