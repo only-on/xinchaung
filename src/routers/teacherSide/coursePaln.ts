0c643443571d27c4e1948e789fd2752cf307d2c5
tianfeng <talongsoft@163.com>
2022-06-09 18:27:35 +0800

    fix: xiufu

--- src/routers/teacherSide/coursePaln.ts
+++ src/routers/teacherSide/coursePaln.ts
@@ -1,12 +1,25 @@
 import RouterViews from "src/components/RouterView.vue";
 import Layout from "src/views/common/Layout.vue";
 import { LocationQuery, RouteParams } from 'vue-router';
+import extStorage from "src/utils/extStorage";
+const { lStorage } = extStorage;
+const role = Number(lStorage.get("role"));
 export default {
   path: "coursePlan",
   component: Layout,
   name:'coursePlan',
   meta: {
-    title: "教学课程",
+    // title: "教学过程",
+    title: (params?: RouteParams, query?: RouteParams) => {
+      console.log(role);
+      
+      const dataDetailMap = {
+          '3': '教学过程',
+          '2': '资源预约',
+      }
+      return dataDetailMap[role.toString()] || dataDetailMap[3]
+      // return dataDetailMap[query!.role && query!.role.toString()] || dataDetailMap[3]
+    },
     authCode: "coursePlan",
   },
   children: [
