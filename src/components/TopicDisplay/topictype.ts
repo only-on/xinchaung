509233189e639ff1cf537d41e06680e9760c7ba0
tianfeng <talongsoft@163.com>
2022-08-02 16:37:25 +0800

    fix: xiufu

--- src/components/TopicDisplay/topictype.ts
+++ src/components/TopicDisplay/topictype.ts
@@ -2,38 +2,6 @@
 import {getThemeData} from 'src/utils/theme'
 const {systemColor} = getThemeData()
 
-const getTopicType2 = () => {
-  return {
-    1: {
-      name: "选择题",
-      
-    },
-    2: {
-      name: "判断题",
-      
-    },
-    3: {
-      name: "填空题",
-      
-    },
-    4: {
-      name: "简答题",
-      
-    },
-    5: {
-      name: "编程题",
-      
-    },
-    6: {
-      name: "模型题",
-      
-    },
-    7: {
-      name: "",
-      
-    },
-  }
-};
 const getTopicType={
     1: {
       name: "选择题",
