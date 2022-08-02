964b24d39d0b4aedad6ec0c761bd793c83345094
shiyao <shiyao@123>
2022-08-02 15:47:42 +0800

    fix: 修改
    Signed-off-by: shiyao <shiyao@123>

--- src/components/answerQuesCom/index.vue
+++ src/components/answerQuesCom/index.vue
@@ -1,40 +1,12 @@
 <template>
     <div class="answerQuesCom">
         <div class="list">
-           <div class="title">{{dataList.title}}</div> 
-           <div class="tip">
-               <div class="tip_name">考试说明</div>
-               <div>交互设计本质上就是设计产品的使用方式的过程，账号怎么填写；表单怎么导出；数据怎么筛选；列表怎么排序等等。针对每个功能的使用方式，都可以花很长的时间去考虑其合理性。一个项目的交互，就是这个项目所有功能使用方式的总和。</div>
-           </div>
-           <div>
-               <div v-for="(item,index) in dataList.list" :key="index">
-                   <div>
-                       <div class="quesType">{{typeNames[item.type-1]}}<span>共 {{}}题 共{{}}分</span></div>
-                       <div class="quesList">
-                           <div v-for="(it,j) in item.quesList" :key="j">
-                            <!-- 单选题 -->
-                            <choice-question v-if="item.type==1" :quesItem='it' :queNumber='j' v-model:selectedAnswer='it.selectedAnswer' ></choice-question>
-                            <!-- 判断题 -->
-                            <judge-question v-if="item.type==2" :quesItem='it' :queNumber='j' v-model:selectedAnswer='it.selectedAnswer' ></judge-question>
-                            <!-- 填空题 -->
-                            <completion-question  v-if="item.type==3" :quesItem='it' :queNumber='j' v-model:selectedAnswer='it.selectedAnswer' ></completion-question>
-                             <!-- 解答题 -->
-                            <explain-question v-if="item.type==4" :quesItem='it' :queNumber='j' v-model:selectedAnswer='it.selectedAnswer' ></explain-question>
-                            <!-- 编程题 -->
-                            <programming v-if="item.type==5"  :quesItem='it' :queNumber='j'></programming>  
-                            <!-- 模型题 -->
-                            <model-question v-if="item.type==6" :quesItem='it' :queNumber='j'></model-question> 
-                        </div>
-                       </div>
-                   </div>
-               </div>
-           </div>
+           <Outline :title="headerObj.title" :explain="headerObj.explain" :explainText="headerObj.explainText" />
+            <TopicDisplay />
         </div>
         <div class="answer_list">
             <div class="answer_list_top">
                 <div class="countdown_div">
-                    <!-- <span></span>分钟
-                    <span></span>秒 -->
                     <a-statistic-countdown
                         title=""
                         :value="deadline"
@@ -76,12 +48,8 @@ import {
 import { useRouter, useRoute } from "vue-router";
 import request from "src/api/index";
 import { Modal, message } from "ant-design-vue";
-import choiceQuestion from './choiceQuestion/index.vue'
-import judgeQuestion from './judgeQuestion/index.vue'
-import completionQuestion from './completionQuestion/index.vue'
-import explainQuestion from './explainQuestion/index.vue'
-import programming from './programming/index.vue'
-import modelQuestion from './modelQuestion/index.vue'
+import TopicDisplay from 'src/components/TopicDisplay/index.vue'
+import Outline from 'src/components/TopicDisplay/outline.vue'
 import dayjs, { Dayjs } from 'dayjs';
 interface Props { 
   dataList:any;
@@ -91,6 +59,11 @@ const props = withDefaults(defineProps<Props>(),{
 });
 const typeNames=['单选题','判断题','填空题','解答题','编程题','模型题']
 const deadline=Date.now() + 1000 * 60 * 60 * 2 + 1000 * 30
+const headerObj:any=reactive({
+  title:'单元测验-《大学计算机基础第3版》第3、4章（一）-计算思维、数值与字符编码',
+  explain:'作业/考试说明',
+  explainText:'交互设计本质上就是设计产品的使用方式的过程，账号怎么填写；表单怎么导出；数据怎么筛选；列表怎么排序等等。针对每个功能的使用方式，都可以花很长的时间去考虑其合理性。一个项目的交互，就是这个项目所有功能使用方式的总和。',
+})
 function onFinish(){
       console.log('finished!');
 };
