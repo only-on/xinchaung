eae04e6589aa0d6cb978d040f5f8d26454ab8680
tianfeng <talongsoft@163.com>
2022-08-02 16:35:51 +0800

    fix: xiufu

---
+++ src/components/TopicDisplay/facilityvalue.ts
@@ -0,0 +1,15 @@
+const facilityvalue={
+  simple:{
+    text:'简单',
+    color:'',
+  },
+  moderate:{
+    text:'适中',
+    color:'',
+  },
+  difficulty:{
+    text:'困难',
+    color:'',
+  }
+}
+export default facilityvalue
