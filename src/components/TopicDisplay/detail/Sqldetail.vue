07db4750b816edd8d4777f6fc9b7edf324611ce1
tianfeng <talongsoft@163.com>
2022-08-03 14:12:29 +0800

    fix: xiufu

---
+++ src/components/TopicDisplay/detail/Sqldetail.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="ModelDetail">
+    <div class="details details1">
+      <div class="detailsTit">题目描述</div>
+      <MarkedEditor v-model="props.info.desc" class="markdown__editor" :preview="true"/>
+    </div>
+    <div class="details details1">
+      <div class="detailsTit">数据表结构</div>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import {
+  defineComponent,
+  ref,
+  onMounted,
+  reactive,
+  Ref,
+  inject,
+  computed,
+  toRefs,
+  watch,
+  defineExpose,
+  defineProps,
+  withDefaults,
+} from "vue";
+import MarkedEditor from "src/components/editor/markedEditor.vue";
+interface Iinfo{
+  desc: string;
+}
+interface Props {
+  info:Iinfo
+}
+const props = withDefaults(defineProps<Props>(), {
+  info:()=>{
+    return {
+      desc:'',
+    }
+  }
+});
+
+// const emit = defineEmits<{
+//   (e: "selectedImage", val: any): void;
+// }>();
+
+</script>
+<style scoped lang="less">
+  .ModelDetail{
+    .details{
+      padding: 14px 0;
+      margin-bottom: 1rem;
+      .detailsTit{
+        font-size: 16px;
+        font-weight: 900;
+        color: var(--black);
+        padding-bottom: 1rem;
+      }
+    }
+  }
+</style>
