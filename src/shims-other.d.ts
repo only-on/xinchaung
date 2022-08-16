2997f637027d2f27130e23ea008bf21c8003d9ca
wangting <wangting@simpleedu.com.cn>
2022-08-16 16:40:14 +0800

    fix: 修复

--- src/shims-other.d.ts
+++ src/shims-other.d.ts
@@ -5,6 +5,7 @@ declare module 'http-build-query';
 declare module 'echarts/extension/dataTool'
 declare module 'vue-codemirror'
 declare module '@codemirror/*'
+declare module 'lodash'
 interface Window {
     _config:{
         TUS_SERVE_IP: string,
